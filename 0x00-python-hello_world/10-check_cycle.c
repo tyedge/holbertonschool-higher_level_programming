2feaf6006ad6439c087bc37f6e82cd2f6467f122
tyedge <tyedge.inc@gmail.com>
2019-05-01 19:07:55 +0000

    Correcting answer

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,17 +12,21 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *ground = list;
-	listint_t *chaser = list;
+	listint_t *chaser = list->next;
 
 	if (list == NULL)
 		return (0);
 	if (list->next == NULL)
 		return (0);
-	while (ground != NULL && chaser != NULL && chaser->next->next != NULL)
+	while (ground != NULL && chaser->next != NULL)
 	{
-		ground = ground->next;
-		chaser = chaser->next->next;
 		if (chaser == ground)
+			return (1);
+		ground = ground->next;
+		if (ground->next == NULL)
+			return (1);
+		chaser = chaser->next->next;
+		if (ground->next == NULL)
 			return (1);
 	}
 	return (0);
