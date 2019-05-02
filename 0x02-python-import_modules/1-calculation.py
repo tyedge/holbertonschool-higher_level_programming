3e4827251904a937ab53490f80884583d446ae47
tyedge <tyedge.inc@gmail.com>
2019-05-02 17:58:49 +0000

    Changed import line

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 
 
 def print_calcs():
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, calc.add(a, b)))
-    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calc.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
 if __name__ == "__main__":
     print_calcs()
