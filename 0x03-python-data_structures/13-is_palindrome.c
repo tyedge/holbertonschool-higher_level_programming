d6316599549ab941285a4d639254dfd45eaaed86
tyedge <tyedge.inc@gmail.com>
2019-05-08 03:50:25 +0000

    Adding files

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+int is_palindrome(listint_t **head)
+{
+
+	return (0);
+}
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    new = []
+    for i in tuple_a[:]:
+        new[0].append = tuple_a[i] + tuple_b[i]
+#        new[1].append = tuple_a[1] + tuple_b[1]
+    return new
