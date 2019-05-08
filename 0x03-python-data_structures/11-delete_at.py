c2897bc03128163765c198b0273361151ff6730e
tyedge <tyedge.inc@gmail.com>
2019-05-08 00:33:16 +0000

    Removed null case

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,9 +2,7 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if not my_list:
-        return None
-    elif idx < 0:
+    if idx < 0:
         return my_list
     elif idx > (len(my_list) - 1):
         return my_list
