ab9ef690664e56d7dd750869c98cd48947569a36
tyedge <tyedge.inc@gmail.com>
2019-05-10 01:56:50 +0000

    Adding answer to file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,3 +2,9 @@
 
 
 def search_replace(my_list, search, replace):
+    newlist = []
+    for i in my_list:
+        if i is search:
+            i = replace
+        newlist.append(i)
+    return newlist
