311f95ad93d00a0c05623ea44116d8cc56cac763
tyedge <tyedge.inc@gmail.com>
2019-05-11 06:42:20 +0000

    Correted edge cases

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,16 @@
 
 
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
-    if type(roman_string) != str:
-        return None
-
     dict = {'I': "1", 'V': "5", 'X': "10", 'L': "50", 'C': "100", 'D': "500",
             'M': "1000"}
     sum = 0
+
+    if not roman_string:
+        return None
+    if type(roman_string) != str:
+        return 0
+    if type(roman_string) != str:
+        return 0
 
     for i in roman_string:
         if i in dict:
