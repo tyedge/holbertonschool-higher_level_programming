1ee7c4f31bcaef884284088566f9e3d54664cbf2
tyedge <tyedge.inc@gmail.com>
2019-05-11 06:36:23 +0000

    Added test case, moved null case

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,15 @@
 
 
 def roman_to_int(roman_string):
+    if not roman_string:
+        return None
+    if type(roman_string) != str:
+        return None
+
     dict = {'I': "1", 'V': "5", 'X': "10", 'L': "50", 'C': "100", 'D': "500",
             'M': "1000"}
     sum = 0
 
-    if not roman_string:
-        return None
     for i in roman_string:
         if i in dict:
             sum += int(dict[i])
