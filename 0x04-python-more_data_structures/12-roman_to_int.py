2e3aa1fbc16597a5b9708c707c34aec6b91f6d3d
tyedge <tyedge.inc@gmail.com>
2019-05-11 06:08:18 +0000

    Adding answer to file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,3 +2,20 @@
 
 
 def roman_to_int(roman_string):
+    dict = {'I': "1", 'V': "5", 'X': "10", 'L': "50", 'C': "100", 'D': "500",
+            'M': "1000"}
+    sum = 0
+
+    if not roman_string:
+        return None
+    for i in roman_string:
+        if i in dict:
+            sum += int(dict[i])
+    if "IV" in roman_string or "IX" in roman_string:
+        sum -= 2
+    if "XL" in roman_string or "XC" in roman_string:
+        sum -= 20
+    if "CD" in roman_string or "CM" in roman_string:
+        sum -= 200
+
+    return sum
