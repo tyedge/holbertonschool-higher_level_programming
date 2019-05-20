dac5977a29f4b5e5da2602bec60be2a1f607e61b
tyedge <tyedge.inc@gmail.com>
2019-05-20 18:39:33 +0000

    Adding answer to file

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+
+    try:
+        print("{:d}".format(value), end='')
+        print('')
+        return True
+
+    except ValueError:
+        return False
