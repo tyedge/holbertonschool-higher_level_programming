961b99f92b893a24927c85e920bb549b490425f6
tyedge <tyedge.inc@gmail.com>
2019-05-20 16:36:13 +0000

    Adding answer to file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,5 @@ def safe_print_list(my_list=[], x=0):
         print('')
         return a
 
-        raise IndexError
-
     except IndexError:
-        print("Oops! Index out of range")
+        print("Index out of range")
