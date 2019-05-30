1d5c526debe335c9bebadf38742579a67051afdf
tyedge <tyedge.inc@gmail.com>
2019-05-30 12:16:00 +0000

    Adding answer to file

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -17,10 +17,38 @@ class Rectangle:
         width (int): the width of of a rectangle instance
         height (int): the height of a rectangle instance
     """
+    number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        type(self).number_of_instances += 1
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        x = ''
+        if self.__width != 0 and self.__height != 0:
+            i = 1
+            while i in range(self.__height):
+                x += '%s\n' % (str(self.print_symbol) * self.__width)
+                i += 1
+            x += '%s' % (str(self.print_symbol) * self.__width)
+        return x
+
+    def __repr__(self):
+        return 'Rectangle(%s, %s)' % (self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
 
     @property
     def width(self):
@@ -38,10 +66,20 @@ class Rectangle:
     def height(self):
         return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_2.area() > rect_1.area():
+            return rect_2
+        return rect_1
