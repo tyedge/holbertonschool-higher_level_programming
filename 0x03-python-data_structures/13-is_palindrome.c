ab5cff12b9cb76a704bd1a1cb6a6221833aa09e0
tyedge <tyedge.inc@gmail.com>
2019-06-01 09:07:46 +0000

    Rewrote answer to improve efficiency

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,8 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 
-listint_t *curr_trav(listint_t *curr, int c_idx);
-listint_t *prev_trav(listint_t *prev, int p_idx);
 int get_len(listint_t *h);
 
 /**
@@ -14,47 +12,39 @@ int get_len(listint_t *h);
  *
  */
 
-
-
 int is_palindrome(listint_t **head)
 {
 	listint_t **getter = head;
-	listint_t *pretrav = *head;
-	listint_t *curtrav = *head;
-	listint_t *previous = NULL;
-	listint_t *current = NULL;
-	int prev_idx, curr_idx;
-	int half, len = 0;
+	listint_t *trav = *head;
+	int store[2048];
+	int len, i = 0, start = 0, end;
 
 	if (head == NULL)
 		return (0);
 	if (*head == NULL)
 		return (1);
 	len = get_len(*getter);
-	if (len % 2 == 0)
+	end = len - 1;
+	if (len == 1)
+		return (1);
+	while (trav->next != NULL)
 	{
-		half = len / 2;
-		prev_idx = half - 1;
-		curr_idx = half;
+		store[i] = trav->n;
+		trav = trav->next;
+		i++;
 	}
-	else
+	store[i++] = '\0';
+	while (start < len && end >= 0)
 	{
-		half = (len - 1) / 2;
-		prev_idx = half - 1;
-		curr_idx = half + 1;
-	}
-	while (prev_idx >= 0 && curr_idx < len)
-	{
-		previous = prev_trav(pretrav, prev_idx);
-		current = curr_trav(curtrav, curr_idx);
-		if (previous->n != current->n)
+		if (start >= end)
+			return (1);
+		if (store[start] != store[end])
 			return (0);
-		prev_idx -= 1;
-		curr_idx += 1;
+		start++;
+		end--;
 	}
 	return (1);
 }
-
 
 
 /**
@@ -78,59 +68,4 @@ int get_len(listint_t *h)
 		numnode = numnode->next;
 	}
 	return (count);
-}
-
-
-/**
- * prev_trav - counts the elements in a linked
- * list
- * @prev: pointer to the first element of the list
- * @p_idx: desired index
- *
- * Return: pointer to node stored at desired index
- *
- */
-
-listint_t *prev_trav(listint_t *prev, int p_idx)
-{
-	listint_t *pre = prev;
-	int i = 0;
-
-	while (i != p_idx)
-	{
-		if (pre->next != NULL)
-		{
-			pre = pre->next;
-			i++;
-		}
-	}
-	return (pre);
-
-}
-
-
-/**
- * curr_trav - counts the elements in a linked
- * list
- * @curr: pointer to the first element of the list
- * @c_idx: desired index
- *
- * Return: pointer to node stored at desired index
- *
- */
-
-listint_t *curr_trav(listint_t *curr, int c_idx)
-{
-	listint_t *cur = curr;
-	int i = 0;
-
-	while (i != c_idx)
-	{
-		if (cur->next != NULL)
-		{
-			cur = cur->next;
-			i++;
-		}
-	}
-	return (cur);
 }
