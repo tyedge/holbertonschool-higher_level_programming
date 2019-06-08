cb124d6e1fe359c23f387cf6d06f4d1b8fc10a7a
tyedge <tyedge.inc@gmail.com>
2019-06-08 20:38:49 +0000

    Adjusted answer

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -8,12 +8,11 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 newlist = []
 filename = "add_item.json"
 i = 1
-save_to_json_file(newlist, filename)
+#save_to_json_file(newlist, filename)
 with open(filename, 'a', encoding='utf-8') as file:
     while i < len(sys.argv):
         newlist.append(sys.argv[i])
         file.write(json.dumps(newlist))
         i += 1
-
 save_to_json_file(newlist, filename)
 load_from_json_file(filename)
