896d4e3896d08d5ac154a09fdb0b6c3bfa390a34
tyedge <tyedge.inc@gmail.com>
2019-06-09 03:51:08 +0000

    Adding answer to file

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+
+class Student:
+    """ The Student Class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.first_name = first_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
