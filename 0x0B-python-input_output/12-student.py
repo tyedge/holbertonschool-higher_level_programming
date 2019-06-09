d8ef81d8158bfc568a1f8b443bf7067e05e603ab
tyedge <tyedge.inc@gmail.com>
2019-06-09 05:18:42 +0000

    Adding answer to file

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+
+class Student:
+    """ The Student Class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dict = self.__dict__
+        if not attrs:
+            return dict
+        else:
+            dict2 = {}
+            for i in attrs:
+                if type(i) is str:
+                    for k, v in dict.items():
+                        if i == k:
+                            dict2[k] = v
+            return dict2
