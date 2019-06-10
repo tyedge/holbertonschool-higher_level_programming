fcd36d71589a67e64bcf84476f8a01f433d41a50
tyedge <tyedge.inc@gmail.com>
2019-06-10 00:21:29 +0000

    Correcting area() function

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,7 +6,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.size = size
+        self.__size = size
 
     def area(self):
-        return self.area()
+        return self.__size * self.__size
