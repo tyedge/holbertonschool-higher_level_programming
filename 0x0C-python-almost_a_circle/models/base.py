bcf6e965b038bd64de42d67138e91682fa0c7bd0
tyedge <tyedge.inc@gmail.com>
2019-06-13 02:24:42 +0000

    Added documentation to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
+""" This module defines the Base class and its attributes """
 
 class Base:
     """ This is the Base Class """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ This function initializes new instances of this class """
         if id is not None:
             self.id = id
         else:
