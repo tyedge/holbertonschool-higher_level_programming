ebfeb5ffa3abf9f4a0b2d9d38fb05fd90f6893eb
tyedge <tyedge.inc@gmail.com>
2019-07-15 23:33:40 +0000

    Adding answer to Task 1

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Write a script that creates the database hbtn_0c_0 in your MySQL server
+-- This script creates a new database and doesn't fail if it already exists
+CREATE database IF NOT EXISTS hbtn_0c_0;
