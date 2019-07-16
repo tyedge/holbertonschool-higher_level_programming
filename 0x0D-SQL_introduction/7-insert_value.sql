115cd2ba4756bf669d9e898ec1399da201f8af97
tyedge <tyedge.inc@gmail.com>
2019-07-16 02:27:10 +0000

    Adding answers to tasks 7-11

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Write a script that inserts a new row in the table first_table
+-- (database hbtn_0c_0) in your MySQL server
+-- This script inserts a row into a table
+INSERT INTO first_table(id, name) VALUES(89, 'Holberton School');
