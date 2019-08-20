462bdcb1429098d31ae04d3f4fac39ec6cf0cea9
tyedge <tyedge.inc@gmail.com>
2019-08-20 20:24:07 +0000

    Removed hard-coded ip address from script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script displays the size of the body of http response
-curl -sw "%{size_download}\n" 34.206.234.184:46075 | sed 's/[^0-9]*//g'
+curl -sw "%{size_download}\n" "$1" | sed 's/[^0-9]*//g'
