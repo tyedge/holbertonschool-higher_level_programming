ace45625d207c2341ca4f604737944ef49fc9cc6
tyedge <tyedge.inc@gmail.com>
2019-08-21 04:00:11 +0000

    Adding answer to Task6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+""" This function returns a peak number from an array, which is defined as a
+number that is greater than its neighbors"""
+
+
+def find_peak(arr=[]):
+    if not arr:
+        return
+    arrlen = len(arr)
+    if arrlen % 2 == 0:
+        arrlen = arrlen - 1
+    start = arr[0]
+    end = arr[arrlen - 1]
+
+    if start < end:
+        mid = round((start + (end - start))/2)
+        if arr[mid] < arr[mid + 1]:
+            start = arr[mid + 1]
+        else:
+            end = arr[mid]
+
+    return start
