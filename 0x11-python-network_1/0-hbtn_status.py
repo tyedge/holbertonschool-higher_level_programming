7539e14649868791f228a2b7cf4d6260a775d459
tyedge <tyedge.inc@gmail.com>
2019-08-26 18:15:34 +0000

    Updated Task0 file so that it can't be imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 #This script that fetches https://intranet.hbtn.io/status and displays a
 #+ a formatted output of the body
-import urllib.request
+if __name__ == "__main__":
 
-url = 'https://intranet.hbtn.io/status'
-r = urllib.request.Request(url)
-with urllib.request.urlopen(r) as response:
-    ret = response.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(ret), ret, ret.decode('utf-8', 'ignore')))
+    import urllib.request
+
+    url = 'https://intranet.hbtn.io/status'
+    r = urllib.request.Request(url)
+    with urllib.request.urlopen(r) as response:
+        ret = response.read()
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content\
+: {}".format(type(ret), ret, ret.decode('utf-8', 'ignore')))
