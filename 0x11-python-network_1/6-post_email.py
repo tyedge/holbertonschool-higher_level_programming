f7c4d352847d258c3c921b1c5732b9396da02fe2
tyedge <tyedge.inc@gmail.com>
2019-08-27 01:19:38 +0000

    Moved the import lines

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This script sends a POST request to the passed URL with the email as a
 parameter, and displays the body of the response (decoded in utf-8)"""
-if __name__ == '__main__':
-    import requests
-    import sys
+import requests
+import sys
 
+if __name__ == '__main__':
     url = sys.argv[1]
     email = {'email': sys.argv[2]}
     ret = requests.post(url, email)
