05402e906563265fc7a7dc52a6a4d626617e1cf6
tyedge <tyedge.inc@gmail.com>
2019-08-27 01:52:26 +0000

    Added data= to field

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """This script sends a POST request to the passed URL with the email as a
 parameter, and displays the body of the response (decoded in utf-8)"""
-import requests
-import sys
+
 
 if __name__ == '__main__':
+    import requests
+    import sys
+
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    ret = requests.post(url, email)
-    print (ret.text)
+    value = {'email': sys.argv[2]}
+    ret = requests.post(url, data=value)
+    print(ret.text)
