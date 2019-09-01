84ab4087dca30147534902f807d1808de9c8cdb3
tyedge <tyedge.inc@gmail.com>
2019-09-01 02:16:35 +0000

    Resubmitting answer for Task6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,20 +3,25 @@
 number that is greater than its neighbors"""
 
 
-def find_peak(arr=[]):
-    if not arr:
-        return
-    arrlen = len(arr)
-    if arrlen % 2 == 0:
-        arrlen = arrlen - 1
-    start = arr[0]
-    end = arr[arrlen - 1]
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+    arrlen = len(list_of_integers)
+    start = 0
+    end = arrlen - 1
+    return peak_finder(list_of_integers, start, end, arrlen)
 
-    while start < end:
-        mid = round((start + (end - start))/2)
-        if arr[mid] < arr[mid + 1]:
-            start = arr[mid + 1]
-        else:
-            end = arr[mid]
 
-    return start
+def peak_finder(arr, start, end, arrlen):
+    """ This function finds the peak and returns to calling function """
+    mid = int(start + (end - start) / 2)
+
+    if ((mid == 0 or arr[mid - 1] <= arr[mid]) and (mid == end or
+                                                    arr[mid + 1] <= arr[mid])):
+        return arr[mid]
+
+    elif (mid > 0 and arr[mid - 1] > arr[mid]):
+        return peak_finder(arr, start, mid - 1, arrlen)
+
+    else:
+        return peak_finder(arr, mid + 1, end, arrlen)
