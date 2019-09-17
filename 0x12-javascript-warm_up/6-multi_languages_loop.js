7d1d22ed1de4949a859b95438a6061bd7b750e78
tyedge <tyedge.inc@gmail.com>
2019-09-17 22:17:09 +0000

    Replaced for each loop with for in loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const myStrings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-myStrings.forEach(function (element) {
-  console.log(element);
-});
+
+const all = myStrings.values();
+
+for (const string of all) {
+  console.log(string);
+}
 /* console.log('C is fun\n' + 'Python is cool\n' + 'Javascript is amazing'); */
