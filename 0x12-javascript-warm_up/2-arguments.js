61d1fc7f4363a581031a60a47604427e3d2b06d5
tyedge <tyedge.inc@gmail.com>
2019-09-17 20:40:37 +0000

    Adding Task2 file

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const count = process.argv.slice(2);
+if (count.length === 0) {
+  console.log('No argument');
+} else if (count.length < 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
