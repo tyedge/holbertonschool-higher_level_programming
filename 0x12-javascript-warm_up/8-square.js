aa1366fdc42f7514aea1cf6435e68f797257f9f5
tyedge <tyedge.inc@gmail.com>
2019-09-17 23:59:41 +0000

    revised to print via console.log

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 
 const num = process.argv[2];
-
 if (process.argv[2] === undefined) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < num; i++) {
+    let text = '';
     for (let j = 0; j < num; j++) {
-      if (j === (num - 1)) {
-        process.stdout.write('X\n');
-      } else {
-        process.stdout.write('X');
-      }
+      text += 'X' + '';
     }
+    console.log(text);
   }
 }
