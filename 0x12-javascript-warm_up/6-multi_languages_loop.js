5fa1d894000cb2729034184b6d9587fa28b9cd61
tyedge <tyedge.inc@gmail.com>
2019-09-17 22:02:49 +0000

    Adding Task6 file

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const myStrings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+myStrings.forEach(function (element) {
+  console.log(element);
+});
+/* console.log('C is fun\n' + 'Python is cool\n' + 'Javascript is amazing'); */
