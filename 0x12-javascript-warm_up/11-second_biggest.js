823e8656314e9cc500ca988866f92f8a174a107d
tyedge <tyedge.inc@gmail.com>
2019-09-18 17:12:36 +0000

    Resubmitting Task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
 let next = 0;
-let arr = process.argv.slice(2);
-if (process.argv.length <= 2) {
-  console.log(0);
-} else if (process.argv.slice(2).length <= 1) {
+let arr = process.argv;
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  arr = arr.sort().reverse();
-  next = arr[1];
+  arr = arr.sort();
+  next = arr[arr.length - 2];
   console.log(parseInt(next));
 }
