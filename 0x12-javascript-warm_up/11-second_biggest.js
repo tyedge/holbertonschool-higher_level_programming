671667c170a484ab336fc4d69577773501a7221e
tyedge <tyedge.inc@gmail.com>
2019-09-18 04:12:43 +0000

    Resubmitting Task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
 
-let max = -Infinity;
-let next = -Infinity;
+let next = 0;
+let arr = process.argv.slice(2);
 if (process.argv.length <= 2) {
   console.log(0);
 } else if (process.argv.slice(2).length <= 1) {
   console.log(0);
 } else {
-  for (let i = 0, n = process.argv.slice(2).length; i < n; i++) {
-    const ret = parseInt(process.argv[i]);
-
-    if (ret >= max) {
-      next = max;
-      max = ret;
-    } else if (ret < max && ret > next) {
-      next = ret;
-    }
-  }
-  console.log(next);
+  arr = arr.sort();
+  next = arr[arr.length - 2];
+  console.log(parseInt(next));
 }
