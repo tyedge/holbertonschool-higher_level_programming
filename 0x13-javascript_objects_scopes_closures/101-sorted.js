43ac822b1f4ef842c88c1adabdf791f2cfb2344e
tyedge <tyedge.inc@gmail.com>
2019-09-23 22:45:39 +0000

    Adding Task12 file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const myDict = require('./101-data.js').dict;
+const newd = {};
+for (const val in myDict) {
+  if (newd[myDict[val]] === undefined) {
+    newd[myDict[val]] = [];
+  }
+  newd[myDict[val]].push(val);
+}
+console.log(newd);
