2a3e7bf2978fefc17d8e6812b7b34dff794482b1
tyedge <tyedge.inc@gmail.com>
2019-09-23 21:22:45 +0000

    Adding Task4 file

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (wdth, hght) {
+    if (wdth > 0 && hght > 0) {
+      this.width = wdth;
+      this.height = hght;
+    }
+  }
+
+  print () {
+    for (let a = 0; a < this.height; a++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const myVar = this.width;
+    this.width = this.height;
+    this.height = myVar;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;
