00d2ec7b3914dfeb7c5385f027f22b0a232e93b6
tyedge <tyedge.inc@gmail.com>
2019-09-23 21:15:58 +0000

    Adding Task3 file

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (wdth, hght) {
+    if (wdth > 0 && hght > 0) {
+      this.width = wdth;
+      this.height = hght;
+    }
+  }
+
+  print () {
+    for (let a = 0; a < this.height; a++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
