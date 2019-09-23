f852d983ea32389783b00ac908b0289045841314
tyedge <tyedge.inc@gmail.com>
2019-09-23 23:40:35 +0000

    Accounted for empty files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 
 const w = fs.createWriteStream(process.argv[4], { flags: 'a' });
 const r = fs.createReadStream(process.argv[2]);
+const stat1 = fs.statSync(process.argv[2]);
+const sizeFile1 = stat1.size;
 const s = fs.createReadStream(process.argv[3]);
+const stat2 = fs.statSync(process.argv[3]);
+const sizeFile2 = stat2.size;
 
-r.pipe(w);
-s.pipe(w);
+if (sizeFile1 > 0) { r.pipe(w); }
+if (sizeFile2 > 0) { s.pipe(w); }
