218a276cc28c82380693d2fbbe60900b8bbd1c2b
tyedge <tyedge.inc@gmail.com>
2019-09-23 23:16:02 +0000

    Adding Task13 file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+var fs = require('fs');
+
+var w = fs.createWriteStream(process.argv[4], { flags: 'a' });
+var r = fs.createReadStream(process.argv[2]);
+var s = fs.createReadStream(process.argv[3]);
+
+r.pipe(w);
+s.pipe(w);
