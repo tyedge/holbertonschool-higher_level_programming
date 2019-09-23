1faf6f18fad6d0b2d3a63b9590a3a2acf2466353
tyedge <tyedge.inc@gmail.com>
2019-09-23 23:23:10 +0000

    Replaced var with const

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-var fs = require('fs');
+const fs = require('fs');
 
-var w = fs.createWriteStream(process.argv[4], { flags: 'a' });
-var r = fs.createReadStream(process.argv[2]);
-var s = fs.createReadStream(process.argv[3]);
+const w = fs.createWriteStream(process.argv[4], { flags: 'a' });
+const r = fs.createReadStream(process.argv[2]);
+const s = fs.createReadStream(process.argv[3]);
 
 r.pipe(w);
 s.pipe(w);
