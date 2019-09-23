8f80ab8e17ce6567d39f2a481fbf2efff1031f8e
tyedge <tyedge.inc@gmail.com>
2019-09-23 21:55:05 +0000

    Adding Task7 and Task8 files

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let myVar = 0;
+  for (let a = 0; a < list.length; a++) {
+    if (list[a] === searchElement) {
+      myVar += 1;
+    }
+  }
+  return myVar;
+};
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const myArr = [];
+  for (let a = 0; a < list.length; a++) {
+    myArr.push(list[a]);
+  }
+  myArr.reverse();
+  return myArr;
+};
