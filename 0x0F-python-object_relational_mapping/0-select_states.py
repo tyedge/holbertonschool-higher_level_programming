9fe86ded3867bd560a3c2fabb2e8b55de2cbf8c6
tyedge <tyedge.inc@gmail.com>
2019-09-23 02:44:08 +0000

    Resubmitting Task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """This module lists all states from the database hbtn_0e_0_usa"""
-import sys
-import MySQLdb
 
-hostname = 'localhost'
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-myConnection = MySQLdb.connect(host=hostname, user=username, passwd=password,
-                               db=database)
+if __name__ == '__main__':
 
+    import sys
+    import MySQLdb
 
-def doQuery(conn):
-    """This function defines the database query"""
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM {}.states ORDER BY states.id ASC".format
-                (database))
-    for i in cur:
-        print(i)
-doQuery(myConnection)
-myConnection.close()
+    hostname = 'localhost'
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    myConnection = MySQLdb.connect(host=hostname, user=username,
+                                   passwd=password, db=database)
+
+    def doQuery(conn):
+        """This function defines the database query"""
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM {}.states ORDER BY states.id ASC".format
+                    (database))
+        for i in cur:
+            print(i)
+    doQuery(myConnection)
+    myConnection.close()
