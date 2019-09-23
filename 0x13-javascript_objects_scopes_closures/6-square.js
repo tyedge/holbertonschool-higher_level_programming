6a471d3e7e1f9a10301319dfd0b487e9ccd74b81
tyedge <tyedge.inc@gmail.com>
2019-09-23 21:45:56 +0000

    Adding Task 6 file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+class Square extends require('./5-square') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let a = 0; a < this.width; a++) {
+        console.log(c.repeat(this.height));
+      }
+    }
+  }
+}
+module.exports = Square;
