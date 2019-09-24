3c5405a72770bb3ad65708fb859abcdb39cdbacb
tyedge <tyedge.inc@gmail.com>
2019-09-24 20:05:45 +0000

    Adding Task4 file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let num = 0;
+    const res = JSON.parse(body).results;
+    let x, y;
+    for (x of res) {
+      for (y of x.characters) { if (y.includes('https://swapi.co/api/people/18/')) { num++; } }
+    }
+    console.log(num);
+  }
+});
