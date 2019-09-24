fd25178d1959c658040feacf68088bee8d99ae2d
tyedge <tyedge.inc@gmail.com>
2019-09-24 18:15:28 +0000

    Adding Task2 file

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const file = process.argv[2];
+
+request(file, function (error, response) {
+  if (error) { console.log(error); }
+  console.log('Code:', response.statusCode);
+});
