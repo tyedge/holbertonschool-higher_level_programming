9f1f969c7ac6452436240cc5b6f70d136ab089b9
tyedge <tyedge.inc@gmail.com>
2019-09-24 21:04:44 +0000

    Resubmitting Task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,13 @@ const request = require('request');
 const url = process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
-
+    console.log(error);
   } else {
     let num = 0;
     const res = JSON.parse(body).results;
     for (const x of res) {
       for (const y of x.characters) {
-        if (y === 'https://swapi.co/api/people/18/' ||
-            y === 'https://swapi.co/api/people/18') { num++; }
+        if (y.includes('/18/')) { num++; }
       }
     }
     console.log(num);
